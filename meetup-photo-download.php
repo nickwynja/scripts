f7107c06ce9b0d1426a3482326dc20ca694aa755
Nick Wynja <nick@nickwynja.com>
2012-07-07 09:51:18 -0400

    Cleaned up a bit

--- meetup-photo-download.php
+++ meetup-photo-download.php
@@ -1,10 +1,10 @@
 <?php
 
-$filename = "Schunemunk Mountain Ridge Loop and The Megaliths_12-06-23";
+$filename = "Harriman Park 12-04-07";
 $ext = ".jpg";
 
 
-$album_id = "7097972";
+$album_id = "7302322";
 $key = "63e4346865432e323c354a214e7a4c";
 $page = 200;
 $offset = 0;
@@ -29,14 +29,12 @@ $xml = simplexml_load_file(rawurlencode($url));
 $i = 1;
 
 foreach($xml->items->item as $item){
-    //Now you can access the 'row' data using $Item in this case 
-    //two elements, a name and an array of key/value pairs
-    echo "Downloading " . $item->highres_link . "\n";
-    //Loop through the attribute array to access the 'fields'.
-    foreach($item->highres_link as $photo){
-      file_put_contents($filename . "_" . $i . $ext, get_data($photo));
-      $i = $i + 1;
+  echo "Downloading " . $item->highres_link . "\n";
+  foreach($item->highres_link as $photo){
+    file_put_contents($filename . "_" . $i . $ext, get_data($photo));
+    $i = $i + 1;
 
-      }
-    }
+  }
+}
+
 ?>
