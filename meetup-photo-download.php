65fac6328716b70e1c9791524ee2d1904df3f2a2
Nick Wynja <nick@nickwynja.com>
2012-07-08 12:17:02 -0400

    Looping through offsets

--- meetup-photo-download.php
+++ meetup-photo-download.php
@@ -1,19 +1,19 @@
 <?php
 
-$filename = "Harriman Park 12-04-07";
-$ext = ".jpg";
+// Filename for the outputted images. They will be appended with a sequential number.
+$filename = "Meetup_Photo";
 
+// The ID for the photo album. Something like meetup.com/GroupName/photos/12345678
+$album_id = "12345678";
 
-$album_id = "7302322";
-$key = "63e4346865432e323c354a214e7a4c";
+// Get your API Key at http://www.meetup.com/meetup_api/key/
+$key = "YOURKEYHERE";
+
 $page = 200;
-$offset = 0;
-$url = "https://api.meetup.com/2/photos.xml?key=" . $key . "&page=" . $page . "&offset=" . $offset . "&photo_album_id=" . $album_id;
+$url = "https://api.meetup.com/2/photos.xml?key=" . $key . "&page=" . $page . "&photo_album_id=" . $album_id;
+$i = 1;
 
-
-
-function get_data($url)
-{
+function get_data($url) {
   $ch = curl_init();
   $timeout = 5;
   curl_setopt($ch,CURLOPT_URL,$url);
@@ -22,19 +22,27 @@ function get_data($url)
   $data = curl_exec($ch);
   curl_close($ch);
   return $data;
-}
-
-$xml = simplexml_load_file(rawurlencode($url));
-
-$i = 1;
-
-foreach($xml->items->item as $item){
-  echo "Downloading " . $item->highres_link . "\n";
-  foreach($item->highres_link as $photo){
-    file_put_contents($filename . "_" . $i . $ext, get_data($photo));
-    $i = $i + 1;
-
   }
+
+for ($offset = 0; $offset >= 0; $offset++) {
+
+  $xml = simplexml_load_file(rawurlencode($url . "&offset=" . $offset));
+  $response  = count($xml->items->item);
+
+  if ($response == 0): {
+    return;
+  }
+  endif;
+
+    foreach($xml->items->item as $item) {
+      
+        echo "Downloading " . $item->highres_link . "\n";
+        foreach($item->highres_link as $photo){
+          file_put_contents($path . $filename . "_" . $i . ".jpg", get_data($photo));
+          $i = $i + 1;
+          
+        }
+    }
 }
 
 ?>
